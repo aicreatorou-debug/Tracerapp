c05fc283cc3a02551d2e8ae902e62aea6a27c373
aicreatorou-debug <aicreatorou@gmail.com>
2026-09-06 19:50:50 +0530

    Add top-level build file with plugins

---
+++ app/build.gradle.kts
@@ -0,0 +1,6 @@
+// Top-level build file. Individual module build files (app/build.gradle.kts)
+// apply these plugins with their own settings.
+plugins {
+    id("com.android.application") version "8.5.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+}
